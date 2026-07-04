b9f071100deed236af1da947155da3df7a495546
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-04 05:44:27 +0500

    code for finding factorial of a number

---
+++ PracticeCodes/factorial.c
@@ -0,0 +1,21 @@
+/*#include <stdio.h>
+void main()
+{
+    int number,factorial = 1, i=1;
+    again:
+    printf("Enter positive number whose factorial you want to calculate : ");
+    scanf("%d",&number);
+    if(number < 0)
+     {
+        printf("Invalid input, negative numbers have no factorial.\n");
+        goto again;
+     }
+    while( i<number)
+       {
+         factorial += factorial*i;
+         i++;
+       }
+
+    printf("The factorial of %d is %d.",number,factorial);   
+
+}*/
