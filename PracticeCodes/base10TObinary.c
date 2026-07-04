9666d8968f2f4c1915652938ab5b84f651fe9bfa
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-04 05:48:24 +0500

    converting a number to its binary form

---
+++ PracticeCodes/base10TObinary.c
@@ -0,0 +1,16 @@
+/*#include<stdio.h>
+int main()
+{
+  int num;printf("Enter a number : ");scanf("%d",&num);
+
+  int remainder=0,place=1,binary_num=0;
+
+  while(num!=0)
+   {
+    remainder=num%2;
+    binary_num=binary_num+remainder*place;
+    place=place*10;
+    num/=2;
+   }
+printf("The binary number is %d",binary_num);
+}*/
