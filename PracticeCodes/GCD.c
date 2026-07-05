03a55c8887d30f955a85c901d488d43fb36f3183
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:38:37 +0500

    finding GCD

---
+++ PracticeCodes/GCD.c
@@ -0,0 +1,23 @@
+/*#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter you numbers : ");
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    while(a!=b)
+    {
+        switch(a>b)
+        {
+            case 1 :
+            a-=b;
+            break;
+            case 0:
+            b-=a;
+            break;
+        }
+
+    }
+    printf("Gcd = %d",a);
+}*/
