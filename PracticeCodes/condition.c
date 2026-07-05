ba53e5b3f4e159b4e24cc4e3ab6b622214e3bb58
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:33:00 +0500

    adding till condition is satisfied

---
+++ PracticeCodes/condition.c
@@ -0,0 +1,17 @@
+
+/*#include <stdio.h>
+void main()
+{
+    int i=0,num=7,result=0; //initialize result to avoid undefined behaviour
+
+
+    while(result<=1000)
+    {      
+
+        printf("%d*%d = %d\n",num,i,result);
+         result = num*i; // Calculate result for the next iteration
+        i++;
+    }
+
+    return;
+}*/
