11497d0dc8775691ecca740304460f9aa7d840c5
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:37:20 +0500

    finding if a year is leap year or not

---
+++ PracticeCodes/leapYear.c
@@ -0,0 +1,14 @@
+/*#include <stdio.h>
+void main()
+{
+    int year;
+    printf("year= ");
+    scanf("%d",&year);
+    if(year % 4==0 ){
+        printf("Leap Year");
+    }
+    else {
+        printf("Not");
+    }
+    
+}*/
