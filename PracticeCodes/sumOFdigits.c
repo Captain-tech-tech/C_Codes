e69b0ecadb61fae7324c3da377fc0190b30f0ce4
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:43:20 +0500

    sum of digits in a number

---
+++ PracticeCodes/sumOFdigits.c
@@ -0,0 +1,22 @@
+/*#include<stdio.h>
+int digits_sum(int num)
+{
+  int sum=0;
+  while(num!=0)
+   {
+    sum+=num%10;
+    num/=10;
+   }
+
+  return sum; 
+}
+int main()
+{
+  int num;printf("Enter a number : ");scanf("%d",&num);
+
+  int sum=digits_sum(num);
+  
+  printf("The sum of the digit in the number is %d.\n",sum);
+  return 0;
+}*/
+
