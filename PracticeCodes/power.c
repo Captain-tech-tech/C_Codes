ab11f2aee24e70f4bdff243c926845eb3897c608
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:37:57 +0500

    finding a number power

---
+++ PracticeCodes/power.c
@@ -0,0 +1,17 @@
+/*#include <stdio.h>
+void main()
+{
+    int a,b,result=1;
+    printf("Enter base : ");
+    scanf("%d",&a);
+
+    printf("Enter exponenet : ");
+    scanf("%d",&b);
+
+    for( int i=1;i<=b;i++)
+    {
+        result = result*a;
+    }
+    printf("THE result is %d",result);
+
+}*/
