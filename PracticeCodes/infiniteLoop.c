ed310b4186b9aee18a60afc50da08ca57df23d4f
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:39:42 +0500

    code showing infinite loop

---
+++ PracticeCodes/infiniteLoop.c
@@ -0,0 +1,21 @@
+
+//important code 
+/*#include <stdio.h>
+void main()
+{
+    for( ; ; );  //loop run infinte times but print nothing ,this code print nothing
+    
+    printf("Hello");
+    
+}*/
+/*#include <stdio.h>
+void main()
+{
+    for(int i = 9; i >= 10 ; i++) // this code prints nothing like the above one
+      {
+        printf("%d ",i);
+
+        printf("\n");
+      }
+    
+}*/
