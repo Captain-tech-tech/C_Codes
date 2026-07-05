677b718a858e72a5d8dd6e56469ef85b72ad5c9d
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:40:31 +0500

    sum of all even numbers

---
+++ PracticeCodes/evenNUMBERsum.c
@@ -0,0 +1,21 @@
+
+// question 1
+/*#include <stdio.h>
+void main()
+{
+    int start=1,given_number,total_sum=0;
+    printf("Enter a number : ");
+    scanf("%d",&given_number);
+    while(start<=given_number)
+    {
+        
+        if(start%2 == 0)
+        {
+            total_sum  += start;
+        }
+        start++;
+
+    }
+     printf("The sum of all even numbers from 1 to %d is %d. ",given_number,total_sum);
+}*/
+
