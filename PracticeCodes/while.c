091d19aae0f059159e64931aa1e1232b095b10eb
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-05 07:35:56 +0500

    while condition use through codes

---
+++ PracticeCodes/while.c
@@ -0,0 +1,62 @@
+
+/*#include <stdio.h>
+void main()
+{
+    int num = 7;
+    int limit = 1000;
+    while (num<=limit)
+    {
+        if (num % 7 == 0)
+        {
+            printf("%d\t", num);
+            // num++;
+        }
+        num++;
+    }
+
+    return;
+}*/
+
+
+//question 2
+/*#include <stdio.h>
+void main()
+{
+    int num = 6;
+    int limit = 1000;
+    int total_sum = 0;
+    while(num<=1000)
+    {
+        num+=2;
+        total_sum += num;
+    }
+
+    printf("Total sum : %d\n",total_sum);
+}*/
+
+/*#include <stdio.h>
+void main()
+{
+    int num = 4,i=1,limit =1000;
+    while(num*i <= limit)
+    {
+        printf("%d ",num*i);
+        i++;
+    }
+    return;
+}*/
+
+/*#include <stdio.h>
+void main()
+{
+    int add = 2;
+    int num = 6;
+    int total_sum =0;
+    while(num<=1000)
+    {
+        num+=add;
+        total_sum+=num;
+    }
+    printf("%d",total_sum);
+
+}*/
