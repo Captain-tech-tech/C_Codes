e911e3aeb7c35127a1a77dad1f0089fac9accb8a
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:26:52 +0500

    switch use in the code

---
+++ PracticeCodes/switch.c
@@ -0,0 +1,26 @@
+/*#include <stdio.h>
+void main()
+{
+    int age = 19;
+       switch(age)    // (age++)    (age+1)   (++age)  (1+age)  (--age)  (age--)
+         {
+            case 19:
+            printf("you are in 19");
+            break;
+
+            case 18 :
+            printf("you are in 18");
+            break;
+
+            case 20 :
+            printf("you are in 20");
+            break;
+
+            default:
+            printf("You have entered some thing invalid");
+         }
+
+         printf("\nThe number is %d",age);
+
+         return;
+}*/
