431d4698adf03f51a8b414c556e01d3f449f9fdb
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:25:13 +0500

    using getchar and putchar

---
+++ PracticeCodes/ASCII.c
@@ -0,0 +1,10 @@
+/*#include <stdio.h>
+void main()
+{
+    char abc = 'a';
+    printf("char abc = %abc\n",abc);   // %p is used to find pointer address   // %abc also show address   ,  if & is used before abc after comma it prints unexpected 
+                                      // or garbage valve
+
+    printf("value of a in ascii is %d",'a');                                  
+    return ;                                  
+}*/
---
+++ PracticeCodes/getcharANDputchar.c
@@ -0,0 +1,23 @@
+//use of getchar and putchar
+/*#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    char ch;
+    printf("Enter a character : ");
+    ch =getchar();
+    printf("The character entered is : ");
+    putchar(ch);
+}
+{
+    char a,b;
+    printf("Enter a character : ");
+    a = getchar();
+    getchar();
+    printf("Enter another character : ");
+    b = getchar();
+
+    printf("the inputs are  %c  and  %c",a,b);
+
+    getchar();
+}*/
