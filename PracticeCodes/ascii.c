cde6c17302e4810a8fe09d82775c9e1d0529dd84
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:23:42 +0500

    finding the desired alphabet

---
+++ PracticeCodes/ascii.c
@@ -0,0 +1,20 @@
+
+/*#include <stdio.h>
+void main()
+{
+    char contact = 'A';
+
+    printf("Searching Contacts...\n");
+    while(1)//infinite loops
+    {
+        printf("Current contact : %c\n",contact);
+
+        if(contact == 'M')
+        {
+            printf("Contact Found ! Now calling ...\n");
+            break;
+        }
+        contact+=1;
+    }
+    return;
+}*/
