b98cbdeb7c733664ec111fc5cc307db3af116758
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:20:05 +0500

    goto and start use

---
+++ PracticeCodes/gotoUSE.c
@@ -0,0 +1,20 @@
+
+/*#include <stdio.h>
+void main()
+{
+    int pin , attempts = 0;
+    start:
+    printf("Enter Pin : ");
+    scanf("%d",&pin);
+
+    if (attempts <= 3)
+    {
+        attempts++;
+        goto start;
+    }
+    else 
+    {
+        printf("Account blocked!\n");
+    }
+    return;
+}*/
