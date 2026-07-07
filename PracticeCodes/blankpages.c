c08e3e25bd43f44af46c5e12e34b456c2c4d492d
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:20:53 +0500

    code for skipping blank pages

---
+++ PracticeCodes/blankpages.c
@@ -0,0 +1,13 @@
+// skipping blank pages
+/*#include <stdio.h>
+void main()
+{
+    for(int page = 1; page <= 20; page++)
+    {
+        if(page%2 != 0)
+
+        printf("Skipping Blank Page #%d...\n",page);
+        continue;
+    }
+    return;
+}*/
