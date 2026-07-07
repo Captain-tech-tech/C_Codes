ab448f1d3aeca93860aa3eacf7cf91dd191613fa
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:17:07 +0500

    code for counting number of setups performed

---
+++ PracticeCodes/setupCOUNT.c
@@ -0,0 +1,14 @@
+/*#include <stdio.h>
+void main()
+{
+    int performed_setups = 0;
+    int required_setups = 10;
+    while(performed_setups<required_setups)
+    {
+        performed_setups++;
+
+        printf("%d Done ! ",performed_setups);
+        printf("%d remaining ...\n",required_setups-performed_setups);
+    }
+    return;
+}*/
