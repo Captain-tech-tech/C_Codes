55c8c6ae453dbd9be24d39cf35be744edeb3b821
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:17:56 +0500

    simple game menu

---
+++ PracticeCodes/gameMenu.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+void main()
+{
+    char exit_pressed = 'N';
+    do
+    {
+        printf("Game menu screen\n");
+        printf("\tplay\n");
+        printf("\tSettings\n");
+        printf("\tExit\n");
+
+        exit_pressed = 'Y';
+    } 
+    while (exit_pressed != 'Y');
+
+    return;
+    
+}*/
