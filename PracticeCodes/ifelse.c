3fb6ec6b33d03d27d7eaa7a754a7406cfde4e24f
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:25:59 +0500

    an import if else concept code

---
+++ PracticeCodes/ifelse.c
@@ -0,0 +1,17 @@
+/*#include <stdio.h>
+void main()
+{
+    int age = 15;
+    if (age >18)
+    //{
+    printf("!st statement\n");     // if curly braces are not used , compiler consider only next line as part of "if" or "else if" or "else"
+    printf("2nd statement\n");
+    //}                                 //if there is no other block of code in else section it will not show error ,here it is optioinal
+                                         //but if it is present we need to use curly braces
+                                            //necessarily
+    else 
+    //printf("3rd statement\n");
+    //printf("4th statement\n");
+
+    return;
+}*/
