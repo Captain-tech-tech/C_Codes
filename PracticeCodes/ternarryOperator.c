3f67784ddcd21ca00fa948cc3001de9eac602bc1
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-07 05:27:42 +0500

    ternary operator use

---
+++ PracticeCodes/ternarryOperator.c
@@ -0,0 +1,11 @@
+
+//tertinary operators
+/*#include <stdio.h>
+void main()
+{
+    printf("start of main\n");
+    int x = 0;
+    x = (100<1)?10:2;  //  (100>1)     (100<1)
+    printf("%d",x);
+    return;
+}*/
