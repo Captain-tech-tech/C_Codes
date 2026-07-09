336ad539851cbd062e2bcc1f1067c5b85d9d5c85
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:16:50 +0500

    calling one function inside another function

---
+++ function_related_codes/functioncallInsideAnother.c
@@ -0,0 +1,31 @@
+/*#include <stdio.h>
+void england()
+{
+    printf("You are now in england\n"); // 9
+    return; // 10
+}
+void australia()
+{
+    printf("you are now in australia\n"); // 7
+    england(); // 8
+    return;// 11
+}
+void america()
+{
+    printf("You are now in america\n");// 5
+    australia(); // 6
+    return; // 12
+}
+void pakistan()
+{
+    printf("You are now in Paksitan\n"); // 3
+    america(); // 4
+    return; // 13
+}
+
+int main()
+{
+    printf("You are now in main function\n");// 1
+    pakistan(); //2
+    return 0;// 14
+}*/
