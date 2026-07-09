9638e549621b4030fa73c3b4d777487535e6cdc0
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:17:43 +0500

    function call inside another function

---
+++ function_related_codes/nestedFunctioncall.c
@@ -0,0 +1,39 @@
+ 
+// shuffling the above code and gaining the same output
+/*#include <stdio.h>
+void australia()
+{
+    printf("you are now in australia\n");
+    void england();
+    england(); 
+    return;
+}
+
+void pakistan()
+{
+    printf("You are now in Paksitan\n"); 
+    void america();
+    america(); 
+    return; 
+}
+
+void america()
+{
+    printf("You are now in america\n");
+    australia(); 
+    return; 
+}
+
+int main()
+{
+    printf("You are now in main function\n");
+    pakistan(); 
+    return 0;
+}
+
+void england()
+{
+    printf("You are now in england\n");
+    // main(); //when main() function is called here infinite loop will execute // for more experiment run main function in 'for loop'
+    return; 
+}*/
