2c54d0368c56a398ed0af7a58d7cfc0ed4f57976
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:19:33 +0500

    using pre-defined math library during function call

---
+++ function_related_codes/maths.c
@@ -0,0 +1,31 @@
+
+// some pre-defined <math.h> library function
+/*#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a = sqrt(49);
+    printf("\n\n%d\n\n",a);
+
+    int b = pow(5,4);
+    printf("%d\n\n",b);
+
+    int c = ceil(34.7);
+    printf("%d\n\n",c);
+
+    int d = floor(34.7);
+    printf("%d",d);
+}*/
+
+//somr pre-defined <ctype.h> libraray functions
+/*#include <stdio.h>
+#include <ctype.h>
+void main()
+{
+    printf("The Uppercase of 'a' is %c\n",(char)toupper('a'));
+    printf("Enter a character(lowercase if answer is non-zero and Uppercase if answer is 0) : ");
+    char a;
+    scanf("%c",&a);
+    printf("The character is %d\n",islower(a));// similarly isupper() is also a function
+    printf("The lowercase of 'A' is %c\n",(char)tolower('A'));
+}*/
