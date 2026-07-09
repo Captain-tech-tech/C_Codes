db6d2ce4f62925c9aaf990428aad7c047f1c3570
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:22:34 +0500

    printing pattern through function

---
+++ function_related_codes/pattern.c
@@ -0,0 +1,14 @@
+
+// printing * throught function call
+/*#include<stdio.h>
+void printstar(int n)
+  {
+    for(int i = 0;i < n;i++)
+      printf("%c",'*');
+  }
+int main()
+{
+    printstar(7);
+    printf("\n");
+    printstar(9);
+}*/
