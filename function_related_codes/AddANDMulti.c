48b2043b93d5a586a324ea24db845f5dd494582b
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:18:40 +0500

    addition and multiplication through function

---
+++ function_related_codes/AddANDMulti.c
@@ -0,0 +1,35 @@
+/*#include<stdio.h>
+int add(int a,int b)
+{
+    return a+b;
+}
+
+void main()
+{
+    int x,y;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &x,&y);
+
+    int sum = add(x,y);
+    printf("The sum of the two numbers is %d.",sum);
+    return;
+}*/
+
+// simple multiplication through function
+/*#include <stdio.h>
+int mul(int a,int b)
+{
+     int c;
+     c = a*b;
+     return c;
+}
+void main()
+{
+    int x,y;
+    printf("Enter two numbers whose multiplication you want to find : ");
+    scanf("%d %d", &x,&y);
+    int z;
+    z = mul(x,y);
+    printf("The multiplication of %d and %d is %d",x,y,z);
+    return;
+}*/
