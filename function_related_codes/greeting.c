4367884590a5fa48ee1f346394f34c7d6aa37fde
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:13:50 +0500

    simple greeting through a function call

---
+++ function_related_codes/greeting.c
@@ -0,0 +1,38 @@
+/*general syntex of function
+returntype functionName(parameters list)
+{ 
+   statements;
+   return value;
+}*/
+
+//simple greeting through function call
+/*#include <stdio.h>
+void greet(){    // this function is called external function
+    printf("Salam\n");
+    printf("How are you bro\n");
+    printf("Are you fine\n");
+    printf("Bye\n");
+    return;
+}
+
+void greeting(char name[])
+{
+    printf("Hello %s, how are you %s",name,name);
+
+    return;
+}
+
+int main()
+{
+    greet();
+    greet();
+
+    printf("\nWe can also call function in loop\n\n");
+
+    for(int i = 0;i <=3;i++)
+    greet();
+
+    greeting("Muhammad Atif");
+
+    return 0;
+}*/
