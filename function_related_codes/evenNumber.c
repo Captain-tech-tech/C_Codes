2da75731b24cd521497e85ce1997acddd7c8b850
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:20:38 +0500

    checking whether a number is even or odd

---
+++ function_related_codes/evenNumber.c
@@ -0,0 +1,23 @@
+/*#include<stdio.h>
+#include<stdbool.h>
+int even(int num)
+{
+    if(num%2 == 0)
+    return true;
+    else
+    return false;
+}
+int main()
+{
+    int num;
+    printf("Enter a number :\n");
+    scanf("%d",&num);
+    int number = even(num);
+
+    if(number)
+    printf("\n%d is an even number\n",num);
+    else
+    printf("%d is not even number\n",num);
+
+    return 0;
+}*/
