d9e0902a4a18eeda81234e4d56aab5d294b30ea5
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:15:30 +0500

    printing  a pattern through a function

---
+++ function_related_codes/patternprinting.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int minimum(int a,int b)
+{
+    int min = a;
+    if(b<a)
+    min=b;
+    return min;
+}
+
+void main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    int min = 0;
+    for(int i = 1;i <= 2*n-1;i++)
+       {
+         for(int j = 1;j <= 2*n-1;j++)
+           {
+            int a = i;
+            if(i>n) 
+            a=2*n-i;
+
+            int b = j;
+            if(b>n)
+            b = 2*n-j;
+            
+            min=minimum(a,b);
+            printf("%d",n+1-min);
+           }
+        printf("\n");   
+       }
+     return;  
+}
