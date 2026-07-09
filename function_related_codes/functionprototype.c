3a3133cee4933ff9d1daf567ec6d89a083895578
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:21:37 +0500

    finding larger and largest number

---
+++ function_related_codes/functionprototype.c
@@ -0,0 +1,25 @@
+
+// function prototype example
+/*#include <stdio.h>
+    // Function prototypes
+    double larger ( double , double );
+    double compareThree ( double x , double y , double z);
+int main() 
+{
+    double a = 5.2,b = 7.8,c = 3.6;
+
+    printf (" Larger of %.2f and %.2f is %.2f\n",a,b,larger(a,b));
+    printf (" Largest of %.2f, %.2f, %.2f is %.2f\n",a,b,c,compareThree(a,b,c));
+    return 0;
+}
+
+double larger ( double x , double y)
+{
+    return (x > y ) ? x : y;
+}
+
+double compareThree ( double x , double y , double z) 
+{
+    double temp = larger (x , y );
+    return larger ( temp , z);
+}*/
