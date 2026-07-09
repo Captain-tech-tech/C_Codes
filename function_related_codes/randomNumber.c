0df307389d6d7ce53fcccc7c8962f19227e1904d
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:23:48 +0500

    printnig random number

---
+++ function_related_codes/randomNumber.c
@@ -0,0 +1,18 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int randomnum(int limit)
+{
+    srand(time(0));
+
+    return rand() % limit;
+}
+void main()
+{
+    int limit;
+    printf("Enter limit for your random number : ");
+    scanf("%d",&limit);
+    int a = randomnum(limit);
+
+    printf("%d",a);
+}*/
