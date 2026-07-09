64739deb9007e05e224cc8315e472b6f231681ea
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-09 07:25:41 +0500

    code to return absolute value of any number

---
+++ function_related_codes/absoluteValue.c
@@ -0,0 +1,16 @@
+/*#include <stdio.h>
+int absValue (int number ) {
+if ( number < 0)
+number = - number ;
+return number ;
+}
+int main () {
+
+int n = -25;
+printf (" Absolute value of %d = %d\n", n , absValue ( n ) ) ;
+return 0;
+
+}*/
+
+
+
