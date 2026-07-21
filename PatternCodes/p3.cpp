262654c65c654a9a9a0a3dbc2f7bfb8e6abbbf11
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-21 23:52:04 +0500

    pattern printing of * and numbers

---
+++ PatternCodes/p3.cpp
@@ -0,0 +1,40 @@
+//  triangle 123*** patterns
+/*#include <stdio.h>
+void main()
+{
+  int height;
+  printf("Enter height of the triangle : ");
+  scanf("%d", &height);
+
+ for(int i = 1;i <= height;i++)
+   {
+    for(int j = height;j > i;j--)
+      printf(" ");
+
+    for(int j = 1;j <= i;j++)
+      printf("%d",j);
+      
+    for(int j = i;j > 1;j--)
+      printf("%d",j-1);
+      
+    printf("\n");  
+   }
+
+   printf("\n\n");
+
+   for(int i = 1;i <= height;i++)
+     {
+      for(int j = height;j > i;j--)
+        printf(" ");
+
+      for(int j = 1;j <= i;j++)
+        printf("%%");
+        
+      for(int j = 1;j <= i;j++)
+        printf("%d",j);
+        
+      printf("\n");  
+     }
+
+     return;
+}*/
