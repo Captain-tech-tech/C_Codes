60a2eca7732267976d73448cfd5308754113ca7c
Captain-tech-tech <mmatif1112@gmail.com>
2026-07-21 23:56:24 +0500

    different triangle pattern

---
+++ PatternCodes/p6.c
@@ -0,0 +1,37 @@
+/*#include <stdio.h>
+void main()
+{
+ int m = 10;
+ int n = 10;
+ for(int i = 1;i <= m;i++)
+   {
+    for(int j = 1;j <= n;j++)
+    printf("%d  ",j*i);
+    printf("\n");
+   }
+}*/
+
+
+ // amazing triangle
+/*#include<stdio.h>
+void main()
+{
+  int i,j,n=5;
+  for(i = 1;i <= n;i++)
+    {
+      for(j=i;j<n;j++)
+       printf(" ");
+
+      for(j=1;j<=(2*i-1);j++)
+      {
+        if(j==1||j==(2*i-1)||i==n)
+          printf("*");
+        else
+          printf(" ");  
+      } 
+      printf("\n");
+    }
+  return;  
+}*/
+
+
